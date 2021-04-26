5c8328c85bf6c4ed7f2d8078cb9e606c0642d9e3
jessijoke <jessijoke@yahoo.com>
2021-04-26 00:09:29 -0400

    commit tree b5cf3403d513e80e93e572a16d0aa50109d658a8

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let mainSucker = document.querySelector('#main');
+
+mainSucker.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "is the champion";
+document.body.appendChild(newHeader);
